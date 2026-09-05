9aae7cf3e70c24267dafb64df50359b751307772
Alex <ademchenko799@mail.ru>
2026-09-05 21:26:00 +0300

    CMakeLists

---
+++ tmp/cmake-build/CMakeFiles/Helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Helloworld.dir/link.d"
+  "CMakeFiles/Helloworld.dir/main.cpp.o"
+  "CMakeFiles/Helloworld.dir/main.cpp.o.d"
+  "Helloworld"
+  "Helloworld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
